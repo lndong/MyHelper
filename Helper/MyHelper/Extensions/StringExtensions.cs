7c8c432f36e2fe659aadbedb0feccd5f2633169f
dong <winterdong2016@outlook.com>
2019-02-25 09:19:24 +0800

    [master] 扩展string Signed-off-by: dong <winterdong2016@outlook.com>

---
+++ Helper/MyHelper/Extensions/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHelper.Extensions
+{
+    /// <summary>
+    /// 字符串扩展类
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 把字符串转换为对应的枚举(忽略字符串大小写)
+        /// </summary>
+        /// <typeparam name="T">枚举</typeparam>
+        /// <param name="source">需要转换的字符串</param>
+        /// <returns></returns>
+        public static T ToEnum<T>(this string source)
+        {
+            return (T) Enum.Parse(typeof(T), source, true);
+        }
+    }
+}
